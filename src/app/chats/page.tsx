ebf8f6f5fa129fd1262e896cb943b34b307a2bae
aemaaeng <haemin120@naver.com>
2023-09-11 23:08:14 +0900

    :card_file_box: api/list 업데이트
    해당 날짜의 채팅에 포함된 미디어 파일의 종류도 함께 리턴하도록 수정

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -1,17 +1,22 @@
 // chatlist
 import Navbar from "@/components/Navbar";
 import ListItem from "@/components/ListItem";
+import { Item } from "@/pages/api/list";
 
 export default async function ChatList() {
-  const resp = await fetch("http://localhost:3000/api/list");
+  const resp = await fetch("http://localhost:3000/api/list", {
+    cache: "no-cache",
+  });
   const list = await resp.json();
 
   return (
     <>
       <Navbar title="CHATS" />
-      {list.map((date: string, index: number) => {
-        return <ListItem content={date} id={index} />;
-      })}
+      <ol>
+        {list.map((item: Item, index: number) => {
+          return <ListItem key={index} content={item.date} id={index} />;
+        })}
+      </ol>
     </>
   );
 }

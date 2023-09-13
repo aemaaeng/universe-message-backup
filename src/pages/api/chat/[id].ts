4d8a25f7b9a2ed7c77ebea02e65c8fbca2f60adc
aemaaeng <haemin120@naver.com>
2023-09-13 21:20:19 +0900

    :zap: 이분탐색 함수로 변경 및 에러 핸들링

--- src/pages/api/chat/[id].ts
+++ src/pages/api/chat/[id].ts
@@ -1,9 +1,45 @@
 // 날짜를 쿼리로 받아서 그 날짜에 해당하는 챗을 보내주는 핸들러
 import type { NextApiRequest, NextApiResponse } from "next";
 import data from "../../../../data.json";
+import { ChatMessage } from "@/components/Bubble";
+
+interface GroupedByDate {
+  date: string;
+  chats: ChatMessage[][];
+}
+
+const assertedData: GroupedByDate[] = data as GroupedByDate[];
+
+function binary_search(
+  arr: GroupedByDate[],
+  target: string,
+  start: number,
+  end: number
+) {
+  const targetDate = new Date(target);
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    const midDate = new Date(arr[mid].date);
+
+    if (String(midDate) === String(targetDate)) {
+      return arr[mid].chats;
+    } else if (midDate < targetDate) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+}
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query;
-  const chat = data.filter((el) => el.date === String(id))[0];
-  res.status(200).json(chat.chats);
+
+  if (typeof id === "string") {
+    const chat = binary_search(assertedData, id, 0, assertedData.length - 1);
+    if (chat) res.status(200).json(chat);
+    else res.status(404).json({ error: "Chat not found" });
+  } else {
+    res.status(404).json({ error: "Invalid Date" });
+  }
 }

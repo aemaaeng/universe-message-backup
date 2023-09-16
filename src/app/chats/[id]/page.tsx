1b5354392139517a86e6d8a6880f8e53ca36de49
aemaaeng <haemin120@naver.com>
2023-09-16 17:00:18 +0900

    :art: fetch 요청 함수로 분리

--- src/app/chats/[id]/page.tsx
+++ src/app/chats/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import GroupedByMin from "@/components/GroupedByMin";
 import { ChatMessage } from "@/components/Bubble";
 import Navbar from "@/components/Navbar";
@@ -6,14 +7,23 @@ type ChatProps = {
   params: { id: string };
 };
 
-export default async function Chats(props: ChatProps) {
-  const id = props.params.id;
+async function fetchChat(id: string) {
   const resp = await fetch(`http://localhost:3000/api/chat/${id}`, {
     headers: {
       Accept: "application/json",
     },
   });
-  const chat = await resp.json();
+  if (!resp.ok) return undefined;
+  return resp.json();
+}
+
+export default async function Chats(props: ChatProps) {
+  const id = props.params.id;
+  const chat = await fetchChat(id);
+
+  if (!chat) {
+    notFound();
+  }
 
   return (
     <>
